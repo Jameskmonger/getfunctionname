71f400f1b60810c2dfd2e32c89e95a3b307116d9
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 14:11:55 +0000

    Test console.log

--- getfunctionname/test.js
+++ getfunctionname/test.js
@@ -2,10 +2,11 @@
 
 var test = require('modunit');
 
-test('it returns the correct function name for a whole function', (t, expected, func) => {
-
+test('it returns the correct function name for a function', (t, expected, func) => {
+    
 }, [
     ['aFunction', function aFunction () { }],
     ['someOtherFunction', function someOtherFunction() { }],
-    ['A_FUNCTION_IN_CAPITALS', function A_FUNCTION_IN_CAPITALS() { }]
+    ['A_FUNCTION_IN_CAPITALS', function A_FUNCTION_IN_CAPITALS() { }],
+    ['log', console.log]
 ]);
