bd9157d1acb739054e8e558d76a136ac09e58f6a
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 14:14:49 +0000

    Add assertion to test

--- getfunctionname/test.js
+++ getfunctionname/test.js
@@ -1,9 +1,12 @@
 "use strict";
 
 var test = require('modunit');
+var getFunctionName = require('./index');
 
 test('it returns the correct function name for a function', (t, expected, func) => {
-    
+    let returned = getFunctionName(func);
+
+    t.assert.equal(expected, returned);
 }, [
     ['aFunction', function aFunction () { }],
     ['someOtherFunction', function someOtherFunction() { }],
