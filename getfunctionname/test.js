600a7f47b96f458b9379241ae17b1f95d9c051e9
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 14:16:35 +0000

    Remove console.log test

--- getfunctionname/test.js
+++ getfunctionname/test.js
@@ -10,6 +10,5 @@ test('it returns the correct function name for a function', (t, expected, func) 
 }, [
     ['aFunction', function aFunction () { }],
     ['someOtherFunction', function someOtherFunction() { }],
-    ['A_FUNCTION_IN_CAPITALS', function A_FUNCTION_IN_CAPITALS() { }],
-    ['log', console.log]
+    ['A_FUNCTION_IN_CAPITALS', function A_FUNCTION_IN_CAPITALS() { }]
 ]);
