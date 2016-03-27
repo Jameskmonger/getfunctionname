4fc684be7235068f6fb0bfeb7460024fbe33395f
oscar-broman <oscar.broman@gmail.com>
2016-03-27 23:48:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-module.exports = function (func) {
-    if (func.name.length === 0) {
-        return 'anonymous';
-    }
-
-    return func.name;
-};
+module.exports = (f) => { return f.name || 'anonymous' };
